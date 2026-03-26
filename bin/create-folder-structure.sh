fbde296715ef7d3ed1d2246290847eb906b3581b
Tristan SCHWENK <tristan.schwenk@ezyostudio.com>
2026-03-26 18:11:36 +0100

    feat: add overseerr

--- bin/create-folder-structure.sh
+++ bin/create-folder-structure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p data/torrents/{movies,tv}
 mkdir -p data/media/{movies,tv}
-mkdir -p docker-config/{vpn,transmission,radarr,sonarr,prowlarr,jellyfin,postgres}
+mkdir -p docker-config/{vpn,transmission,radarr,sonarr,prowlarr,jellyfin,postgres, overseerr}
 
